9cac91756ce3ca2b90d8d11924b3cbd6ece4b560
databuhm <databuhm@gmail.com>
2024-12-09 18:09:09 +0900

    modify first file

--- HackerRank/Revising the Select Query I.sql
+++ HackerRank/Revising the Select Query I.sql
@@ -3,7 +3,6 @@ Query all columns for all American cities in the CITY table with populations lar
 The CountryCode for America is USA.
 
 The CITY table is described as follows:
-*/
 
 CREATE TABLE CITY (
     ID NUMBER,
@@ -13,10 +12,9 @@ CREATE TABLE CITY (
     POPULATION NUMBER
 )
 ;
-
-/*
-Solution
 */
+
+-- Solution
 
 SELECT *
 FROM city
